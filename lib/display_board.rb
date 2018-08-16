470468cef61000d6fac85cf338e2a5203b429f2e
Andranique Boone <andranique.boone@gmail.com>
2018-08-16 20:27:36 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board = [ " ", " ", " ", " ", " ", " ", " ", " ", " " ]
+board = [ " ", " ", "O", " ", "O", " ", "O", " ", " " ]
 def display_board(board)
-  print " #{board} "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
