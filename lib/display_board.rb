5694fb5d0aefc3a609d282911aa5a00b9e461f82
Andranique Boone <andranique.boone@gmail.com>
2018-08-16 19:58:32 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,5 +2,5 @@
 # out the current state.
 board = ["X", "0", "0", "0", "X", "O", "O", "O", "X"]
 def display_board(board)
-  puts "#{board[0]}|#{board[4]}| #{board[8]}"
+  puts "#{board[0]} | #{board[4]} | #{board[8]}"
 end
